bf2f2c97ee3c942495980526e7a8a7625c45c242
blitzen23 <michaelthe23@gmail.com>
2023-03-24 17:35:51 +0900

    update responsive contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,15 +7,15 @@ const Contact = () => {
   return (
     <div
       id='contact'
-      className='flex flex-col justify-center items-center bg-secondBackground text-textColor py-24 w-screen px-12'
+      className='flex flex-col justify-center items-center bg-secondBackground text-textColor py-24'
     >
-      <h1 className='text-5xl font-bold mb-2 text-center'>My Contact</h1>
-      <p className='text-xl mb-10 text-gray-400'>
+      <h1 className='text-5xl font-bold'>My Contact</h1>
+      <p className='text-xl text-gray-400 mb-10 mt-2 text-center'>
         Click to directly contact me
       </p>
-      <div className='flex flex-wrap justify-center xl:flex-nowrap md:flex-row flex-col xl:items-center gap-x-12 gap-y-10 xl:grid xl:grid-cols-3'>
+      <div className='flex flex-wrap xl:flex-nowrap md:flex-row flex-col justify-center xl:items-center gap-x-12 gap-y-10 px-5 xl:grid xl:grid-cols-3 w-full'>
         <a
-          className='md:w-2/5 xl:w-auto flex flex-col justify-center items-center text-xl bg-primaryBackground rounded-md hover:shadow-[0_0_2px_2px_#0ef] transition-all ease-in duration-200 mx-5'
+          className='md:w-2/5 xl:w-auto flex flex-col justify-center items-center text-xl bg-primaryBackground rounded-md hover:shadow-[0_0_2px_2px_#0ef] transition-all ease-in duration-200 w-full'
           href='mailto:michaelthe23@gmail.com'
           target='_blank'
           rel='noreferrer noopener'
@@ -25,7 +25,7 @@ const Contact = () => {
           <p className='mx-16 md:mx-24 mb-16'>michaelthe23@gmail.com</p>
         </a>
         <a
-          className='md:w-2/5 xl:w-auto flex flex-col justify-center items-center text-xl bg-primaryBackground rounded-md hover:shadow-[0_0_2px_2px_#0ef] transition-all ease-in duration-200 mx-5'
+          className='md:w-2/5 xl:w-auto flex flex-col justify-center items-center text-xl bg-primaryBackground rounded-md hover:shadow-[0_0_2px_2px_#0ef] transition-all ease-in duration-200 w-full'
           href='tel:6282280298885'
           target='_blank'
           rel='noreferrer noopener'
@@ -35,7 +35,7 @@ const Contact = () => {
           <p className='mx-16 md:mx-24 mb-16'>+6282280298885</p>
         </a>
         <a
-          className='md:w-2/5 xl:w-auto flex flex-col justify-center items-center text-xl bg-primaryBackground rounded-md hover:shadow-[0_0_2px_2px_#0ef] transition-all ease-in duration-200 mx-5'
+          className='md:w-2/5 xl:w-auto flex flex-col justify-center items-center text-xl bg-primaryBackground rounded-md hover:shadow-[0_0_2px_2px_#0ef] transition-all ease-in duration-200 w-full'
           href='https://wa.me?6282280298885'
           target='_blank'
           rel='noreferrer noopener'
